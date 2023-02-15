3e13df2b7e8eeac7a4169fbe2c19a8fb5ab4126b
张旭 <zhangxu@MacBook-Pro.local>
2023-02-15 17:51:13 +0800

    “update“

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,38 +1,21 @@
-import { HardhatUserConfig } from "hardhat/config";
-
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
 
-// dynamically changes endpoints for local tests
-const zkSyncTestnet =
-  process.env.NODE_ENV == "test"
-    ? {
-        url: "http://localhost:3050",
-        ethNetwork: "http://localhost:8545",
-        zksync: true,
-      }
-    : {
-        url: "https://zksync2-testnet.zksync.dev",
-        ethNetwork: "goerli",
-        zksync: true,
-      };
-
-const config: HardhatUserConfig = {
+module.exports = {
   zksolc: {
     version: "1.3.1",
     compilerSource: "binary",
     settings: {},
   },
-  defaultNetwork: "zkSyncTestnet",
+  defaultNetwork: "zkTestnet",
   networks: {
-    hardhat: {
-      zksync: false,
+    zkTestnet: {
+      url: "https://zksync2-testnet.zksync.dev", // URL of the zkSync network RPC
+      ethNetwork: "goerli", // Can also be the RPC URL of the Ethereum network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
     },
-    zkSyncTestnet,
   },
   solidity: {
-    version: "0.8.16",
+    version: "0.8.17",
   },
 };
-
-export default config;
